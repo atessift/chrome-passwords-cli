fe129bcad5f0c024224c64f33b93f92da35694b5
Akos Putz <akos.putz@gmail.com>
2023-03-08 20:39:14 -0800

    Added pycryptodome as requirement

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@
 
 from setuptools import setup
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 setup(
-  name='chrome-pass',
-  version=__version__,
-  author='Dmytro Sadovnychyi',
-  author_email='pip@dmit.ro',
-  url='https://github.com/sadovnychyi/chrome-passwords-cli',
-  description='Access your Chrome passwords from CLI.',
-  scripts=['pass'],
-  keywords='chrome password',
-  license='MIT',
-  install_requires=[
-    'percol >= 0.2.1',
-    'pyperclip >= 1.6.4',
-    'keyring >= 13.2.1',
-  ]
+    name='chrome-pass',
+    version=__version__,
+    author='Dmytro Sadovnychyi',
+    author_email='pip@dmit.ro',
+    url='https://github.com/sadovnychyi/chrome-passwords-cli',
+    description='Access your Chrome passwords from CLI.',
+    scripts=['pass'],
+    keywords='chrome password',
+    license='MIT',
+    install_requires=[
+        'percol >= 0.2.1',
+        'pyperclip >= 1.6.4',
+        'keyring >= 13.2.1',
+        'pycryptodome >= 3.17',
+    ]
 )
